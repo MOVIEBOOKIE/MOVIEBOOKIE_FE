9d645df9bab1539d6375762cc5ce78468e9a8dc8
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-04-22 19:25:20 +0900

    chore: 다자인시스템 테스트코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
 export default function Home() {
-  return (
-    <>
-      <div className="text-red-200">커스텀 컬러 테스트</div>
-      <div className="text-red-main">커스텀 컬러 테스트</div>
-      <h1 className="title-1-bold">타이틀 Title</h1>
-      <p className="body-2-medium">본문 Body</p>
-      <small className="caption-2-regular">캡션 caption</small>
-      <div className="container-padding">margin 20</div>
-    </>
-  );
+  return <div>home</div>;
 }
