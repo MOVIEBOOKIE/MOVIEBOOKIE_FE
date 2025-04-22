953aafbcd74a8b3bba69c12afec819aa57481cdb
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-04-22 19:00:54 +0900

    chore: color,Typography 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <main className="p-4"></main>;
+  return (
+    <>
+      <div className="text-red-200">커스텀 컬러 테스트</div>
+      <div className="text-red-main">커스텀 컬러 테스트</div>
+      <h1 className="title-1-bold">타이틀 Title</h1>
+      <p className="body-2-medium">본문 Body</p>
+      <small className="caption-2-regular">캡션 caption</small>
+    </>
+  );
 }
