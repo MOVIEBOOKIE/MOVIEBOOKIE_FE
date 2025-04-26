ad8bb10e1bc9c5a410c2f4744eec4f3f95dd7bee
Sohyun Park <108279922+Sohyunnnn@users.noreply.github.com>
2025-04-26 19:41:33 +0900

    [Setting/#9] PWA 설정 및  레이아웃 설정 (#11)
    * style: 스타일 수정
    
    * chore: add next-pwa
    
    * chore: pwa 설정
    
    * style: 레이아웃 설정
    
    * style: 디자인시스템 수정
    
    * style: 스타일 수정
    
    * style: 컬러 수정
    
    * style: 스타일 수정
    
    * style: 코드 스타일 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,9 @@ import { ReactNode } from "react";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
