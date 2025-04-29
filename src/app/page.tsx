84c8dcd91576d789763f42af84072e5530248cca
Sohyunnnn <thgus05061@naver.com>
2025-04-30 02:06:31 +0900

    feat: 메인 페이지 퍼블리싱

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+"use client";
+
 import Carousel from "./(route)/home/_components/carousel";
+import ArrowIcon from "@/icons/arrow.svg";
 
 export default function Home() {
   return (
-    <div className="mt-15.75 h-screen">
+    <div className="flex h-full flex-col items-center pt-15.75">
+      <div className="mb-7 flex flex-col items-center">
+        <p className="body-1-medium text-gray-300">못말리는 영화러버</p>
+        <h2 className="title-1-bold text-gray-white mt-0.75">
+          서현님을 위한 추천
+        </h2>
+      </div>
       <Carousel />
+      <div className="mt-10 flex flex-col items-center gap-1">
+        <p>더 많은 이벤트를 찾으려면 아래로 스와이프 </p>
+        <ArrowIcon className="h-6 w-6" />
+      </div>
     </div>
   );
 }
