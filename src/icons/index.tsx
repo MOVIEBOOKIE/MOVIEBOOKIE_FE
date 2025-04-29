2b2e82c6af3b60e8019929b8d5ced11924e41627
Sohyunnnn <thgus05061@naver.com>
2025-04-30 02:05:57 +0900

    asset: asset 추가

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,3 +1,4 @@
 export { default as HomeIcon } from "./home.svg";
 export { default as EventIcon } from "./event.svg";
 export { default as MyIcon } from "./my.svg";
+export { default as ArrowIcon } from "./arrow.svg";
