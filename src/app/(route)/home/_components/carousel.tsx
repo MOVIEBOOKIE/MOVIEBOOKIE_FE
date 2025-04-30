72707eaf96913d5a532f3ff5fe61b1018b97595c
Sohyunnnn <thgus05061@naver.com>
2025-04-30 10:15:30 +0900

    chore: 파일 분리

--- src/app/(route)/home/_components/carousel.tsx
+++ src/app/(route)/home/_components/carousel.tsx
@@ -7,14 +7,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import Image from "next/image";
 import Badge from "@/app/_components/badge";
-
-const MOVIE_SLIDES = [
-  { id: 1, title: "태풍이 지나가고", image: "/image.png" },
-  { id: 2, title: "블루 자켓", image: "/image.png" },
-  { id: 3, title: "마지막 여행", image: "/image.png" },
-  { id: 4, title: "달빛 아래", image: "/image.png" },
-  { id: 5, title: "어둠의 경계", image: "/image.png" },
-];
+import { MOVIE_SLIDES } from "@/app/_mocks/movie-slides";
 
 export default function Carousel() {
   const swiperRef = useRef<SwiperCore | null>(null);
