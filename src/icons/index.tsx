69a0aaa26c2488a06ffec4d558bc9c1f7abec5d2
Sohyunnnn <thgus05061@naver.com>
2025-05-01 19:18:36 +0900

    asset: add asset

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -2,3 +2,4 @@ export { default as HomeIcon } from "./home.svg";
 export { default as EventIcon } from "./event.svg";
 export { default as MyIcon } from "./my.svg";
 export { default as SwipeDownIcon } from "./swipe-down.svg";
+export { default as SearchIcon } from "./search.svg";
