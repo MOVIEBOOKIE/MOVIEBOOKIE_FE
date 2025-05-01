785e1560ae76ee87e4fda7d0eb82ec950174841e
Sohyunnnn <thgus05061@naver.com>
2025-05-01 19:02:19 +0900

    feat: 스와이프 기능 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,32 @@
 
 import { SwipeDownIcon } from "@/icons";
 import Carousel from "./(route)/home/_components/carousel";
+import { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [isFirstScreen, setIsFirstScreen] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!containerRef.current) return;
+      const scrollTop = containerRef.current.scrollTop;
+      const screenHeight = window.innerHeight;
+      setIsFirstScreen(scrollTop < screenHeight / 2);
+    };
+
+    const el = containerRef.current;
+    if (el) el.addEventListener("scroll", handleScroll);
+    return () => el?.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div>
-      <div className="flex h-full flex-col items-center overflow-x-hidden pt-15.75">
+    <div
+      ref={containerRef}
+      className="h-screen snap-y snap-mandatory snap-start overflow-y-scroll scroll-smooth"
+    >
+      <section className="flex h-screen snap-start flex-col items-center overflow-x-hidden pt-15.75">
         <div className="mb-7 flex flex-col items-center">
           <p className="body-1-medium text-gray-300">못말리는 영화러버</p>
           <h2 className="title-1-bold text-gray-white mt-0.75">
@@ -14,14 +35,33 @@ export default function Home() {
           </h2>
         </div>
         <Carousel />
-        <div className="from-gray-black/0 to-gray-black fixed bottom-0 z-10 mb-25.5 flex w-full flex-col items-center gap-1 bg-gradient-to-b from-0% to-50% pt-14.25 pb-3">
-          <p className="caption-1-medium">
+
+        <motion.div
+          className="from-gray-black/0 to-gray-black fixed bottom-0 z-10 mb-25.5 flex w-full flex-col items-center gap-1 bg-gradient-to-b from-0% to-50% pt-14.25 pb-3"
+          initial={{ opacity: 1 }}
+          animate={{ opacity: isFirstScreen ? 1 : 0 }}
+          transition={{ duration: 0 }}
+        >
+          <p className="caption-1-medium text-gray-white opacity-47">
             더 많은 이벤트를 찾으려면 아래로 스와이프
           </p>
           <SwipeDownIcon className="h-6 w-6" />
+        </motion.div>
+      </section>
+
+      <motion.section
+        initial={{ opacity: 0 }}
+        animate={{ opacity: isFirstScreen ? 0 : 1 }}
+        transition={{ duration: 0.6 }}
+        className="flex h-full w-full snap-start flex-col px-5"
+      >
+        <div className="mt-6.25 mb-4 flex flex-col items-center gap-1.25">
+          <SwipeDownIcon className="h-6 w-6 rotate-180" />
+          <p className="caption-1-medium text-white opacity-47">
+            맞춤 이벤트 추천은 위로 스와이프
+          </p>
         </div>
-      </div>
-      <div>스와이프 후 화면</div>
+      </motion.section>
     </div>
   );
 }
