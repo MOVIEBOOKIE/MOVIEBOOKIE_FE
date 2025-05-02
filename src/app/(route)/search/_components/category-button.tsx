c37bd4497ce3d3787880f2420ff41f09635ec75e
Sohyunnnn <thgus05061@naver.com>
2025-05-03 00:05:48 +0900

    feat: 검색 페이지 퍼블리싱

---
+++ src/app/(route)/search/_components/category-button.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface CategoryButtonProps {
+  label: string;
+  src: string;
+  path: string;
+}
+
+export default function CategoryButton({
+  label,
+  src,
+  path,
+}: CategoryButtonProps) {
+  const router = useRouter();
+
+  const handleNavigate = () => {
+    router.push(path);
+  };
+
+  const formattedLabel = label.split("/n").map((line, index) => (
+    <span key={index}>
+      {line}
+      {index < label.split("/n").length - 1 && <br />}
+    </span>
+  ));
+
+  return (
+    <button className="relative" onClick={handleNavigate}>
+      <Image width={161} height={35} src={src} alt={label} />
+      <p className="absolute top-4 left-4 text-start text-gray-100">
+        {formattedLabel}
+      </p>
+    </button>
+  );
+}
