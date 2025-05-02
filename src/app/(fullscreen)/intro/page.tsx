30b1557b7a02d5614f04c277ed989b42c785d38e
HYERYEON <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-02 23:18:41 +0900

    [Refactor/#22] 하단바 레이아웃 분리 (#23)
    * refactor: 레이아웃 분리
    
    * refactor: 코드스타일 수정
    
    * chore: workflows 수정
    
    * fix: style 파일 누락 수정

---
+++ src/app/(fullscreen)/intro/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function intro() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <h1 className="title-1-bold">하단바 test</h1>
+    </main>
+  );
+}
