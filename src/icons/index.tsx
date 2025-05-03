3ab6dc17a3f0106bbce9dbac2d9a623c77bd1c47
Sohyunnnn <thgus05061@naver.com>
2025-05-04 00:01:14 +0900

    asset: add assets

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -10,3 +10,16 @@ export { default as CamcorderIcon } from "./camcorder.svg";
 export { default as PopcornIcon } from "./popcorn.svg";
 export { default as SmileIcon } from "./smile.svg";
 export { default as TelevisionIcon } from "./television.svg";
+
+//trait
+export { default as DissinessIcon } from "./trait/dizziness.svg";
+export { default as GrinIcon } from "./trait/grin.svg";
+export { default as PartyIcon } from "./trait/party.svg";
+export { default as SadIcon } from "./trait/sad.svg";
+export { default as YawnIcon } from "./trait/yawn.svg";
+export { default as DesertIcon } from "./trait/desert.svg";
+export { default as DocumentIcon } from "./trait/document.svg";
+export { default as SpeakingIcon } from "./trait/speaking.svg";
+export { default as SpeechBubbleIcon } from "./trait/speech-bubble.svg";
+export { default as FireIcon } from "./trait/fire.svg";
+export { default as TargetIcon } from "./trait/target.svg";
