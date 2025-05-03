268290a95927e6cbc6f208c4502ae8cb78128f83
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-03 14:00:05 +0900

    fix: 폰트 적용안되는 문제 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import { ReactNode } from "react";
 import "@/styles/globals.css";
+import { pretendard } from "../app/fonts/pretendard";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pretendard.variable}>
       <body>{children}</body>
     </html>
   );
