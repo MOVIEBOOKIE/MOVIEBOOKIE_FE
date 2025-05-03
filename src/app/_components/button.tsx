4873ad2183597ee175d3fc20a32a08b40df1a3c1
Sohyunnnn <thgus05061@naver.com>
2025-05-03 22:44:14 +0900

    refactor: 버튼 variant 추가

--- src/app/_components/button.tsx
+++ src/app/_components/button.tsx
@@ -4,14 +4,25 @@ import { cn } from "../_utils/cn";
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: string;
   className?: string;
+  variant?: "primary" | "secondary";
 }
 
-export default function Button({ children, className, ...props }: ButtonProps) {
+export default function Button({
+  children,
+  className,
+  variant = "primary",
+  ...props
+}: ButtonProps) {
+  const buttonStyles =
+    variant === "primary"
+      ? "bg-red-main text-gray-white"
+      : "bg-gray-950 text-gray-300";
   return (
     <button
       type="button"
       className={cn(
-        "body-3-medium w-full rounded-xl bg-gray-950 py-4 text-gray-300",
+        "body-3-medium w-full rounded-xl py-4",
+        buttonStyles,
         className,
       )}
       {...props}
