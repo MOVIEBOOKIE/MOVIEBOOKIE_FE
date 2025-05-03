fe1f280573199d10e3b1dbf63e6331761d51936b
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-03 13:52:09 +0900

    feat: 하단바 레이아웃 css 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,7 @@ import "@/styles/globals.css";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
-      <body>
-        <main className="mx-auto mb-25.5 h-[calc(100vh-102px)] overflow-y-auto">
-          {children}
-        </main>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
