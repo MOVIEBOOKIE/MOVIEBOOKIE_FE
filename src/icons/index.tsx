4a56dbb92c566388ae13d57b9c41a19aa64d9091
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-03 21:13:07 +0900

    feat: tab notifications 추가

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,7 +1,9 @@
 export { default as HomeIcon } from "./tab/home.svg";
 export { default as EventIcon } from "./tab/event.svg";
 export { default as MyIcon } from "./tab/my.svg";
+export { default as NotificationIcon } from "./tab/Notification.svg";
+export { default as LightIcon } from "./tab/light.svg";
+
 export { default as SwipeDownIcon } from "./ui/swipe-down.svg";
 export { default as SearchIcon } from "./ui/search.svg";
-export { default as LightIcon } from "./tab/light.svg";
 export { default as BackIcon } from "./common/back.svg";
