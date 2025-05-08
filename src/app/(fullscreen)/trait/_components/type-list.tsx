03efff89f59224782dff97844f60a32804cd4a94
Sohyunnnn <thgus05061@naver.com>
2025-05-09 03:27:38 +0900

    refactor: props 추가

--- src/app/(fullscreen)/trait/_components/type-list.tsx
+++ src/app/(fullscreen)/trait/_components/type-list.tsx
@@ -1,18 +1,26 @@
 import { cn } from "@/utils/cn";
-import { ReactNode } from "react";
+import { MouseEventHandler, ReactNode } from "react";
 
 interface TypeListProps {
   children: ReactNode;
   className?: string;
+  onClick?: MouseEventHandler<HTMLDivElement>;
 }
 
-export default function TypeList({ children, className }: TypeListProps) {
+export default function TypeList({
+  children,
+  className,
+  onClick,
+  ...props
+}: TypeListProps) {
   return (
     <div
       className={cn(
         "body-3-regular flex w-full items-center gap-3 rounded-xl border border-gray-900 text-gray-100",
         className,
       )}
+      onClick={onClick}
+      {...props}
     >
       {children}
     </div>
