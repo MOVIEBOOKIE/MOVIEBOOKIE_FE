bc2b5239878ffd2c301a58c2edcc37ac1d96cb99
Sohyunnnn <thgus05061@naver.com>
2025-05-09 02:40:41 +0900

    feat: step1 퍼블리싱

---
+++ src/app/(fullscreen)/trait/_components/step1.tsx
@@ -0,0 +1,32 @@
+import { StepHeader } from "@/components";
+import TypeList from "./type-list";
+import { MOOD_MAP } from "@/constants/trait";
+
+interface Step1Props {
+  nickname: string;
+}
+
+export default function Step1({ nickname }: Step1Props) {
+  return (
+    <div className="mt-8 w-full">
+      <StepHeader
+        StepHeader="1/3"
+        title={
+          <>
+            요즘 {nickname}님의 일상은
+            <br />
+            어떤 느낌인가요?
+          </>
+        }
+      />
+      <div className="flex flex-col gap-2">
+        {Object.entries(MOOD_MAP).map(([key, { icon, text }]) => (
+          <TypeList key={key} className="px-3.75 py-4.5">
+            {icon}
+            <span>{text}</span>
+          </TypeList>
+        ))}
+      </div>
+    </div>
+  );
+}
