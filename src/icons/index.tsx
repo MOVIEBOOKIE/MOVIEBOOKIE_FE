a667d1707fab137d026286979590c16efa32f3e5
Sohyunnnn <thgus05061@naver.com>
2025-05-10 02:37:47 +0900

    asset: add asset

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,23 +1,26 @@
+//tab
 export { default as HomeIcon } from "./tab/home.svg";
 export { default as EventIcon } from "./tab/event.svg";
 export { default as MyIcon } from "./tab/my.svg";
 export { default as NotificationIcon } from "./tab/notification.svg";
 export { default as LightIcon } from "./tab/light.svg";
 
+//ui
 export { default as SwipeDownIcon } from "./ui/swipe-down.svg";
 export { default as SearchIcon } from "./ui/search.svg";
 export { default as DefaultProfileIcon } from "./ui/default-profile.svg";
 export { default as KakaoIcon } from "./ui/kakao.svg";
-
-export { default as BackIcon } from "./common/back.svg";
-export { default as CloseIcon } from "./common/close.svg";
-export { default as LogoWhiteIcon } from "./common/logo-white.svg";
-
 export { default as BallIcon } from "./ball.svg";
 export { default as CamcorderIcon } from "./camcorder.svg";
 export { default as PopcornIcon } from "./popcorn.svg";
 export { default as SmileIcon } from "./smile.svg";
 export { default as TelevisionIcon } from "./television.svg";
+
+//common
+export { default as BackIcon } from "./common/back.svg";
+export { default as CloseIcon } from "./common/close.svg";
+export { default as LogoWhiteIcon } from "./common/logo-white.svg";
+export { default as UploadIcon } from "./common/upload.svg";
 
 //trait
 export { default as DizzinessIcon } from "./trait/dizziness.svg";
