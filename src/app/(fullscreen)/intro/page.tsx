973e7a6b2a23da5dee113e6c0db7ee530d522363
Sohyunnnn <thgus05061@naver.com>
2025-05-09 20:20:07 +0900

    style: 스타일 수정

--- src/app/(fullscreen)/intro/page.tsx
+++ src/app/(fullscreen)/intro/page.tsx
@@ -8,11 +8,7 @@ export default function Intro() {
     <main className="relative h-screen w-full bg-[url('/images/custom-bg.png')] bg-cover bg-center">
       <div className="absolute top-[47.14%] left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
         <div className="flex flex-col items-center">
-          <LogoWhiteIcon
-            width={100}
-            height={100}
-            className="absolute top-[330px] left-1/2 -translate-x-1/2"
-          />
+          <LogoWhiteIcon width={100} height={100} />
           <h1 className="body-2-medium -mt-2 text-white">
             당신의 일상을 영화처럼
           </h1>
