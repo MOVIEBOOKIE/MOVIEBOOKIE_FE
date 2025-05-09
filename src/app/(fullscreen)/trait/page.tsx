f4ab9e404c9bc70f11dac3908a66c5b29cf86803
Sohyunnnn <thgus05061@naver.com>
2025-05-09 13:59:07 +0900

    refactor: 페이지 구조 수정

--- src/app/(fullscreen)/trait/page.tsx
+++ src/app/(fullscreen)/trait/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Step0 from "./_components/step0";
-import { Button } from "@/components";
+import { Button, Header } from "@/components";
 import Step1 from "./_components/step1";
 import Step2 from "./_components/step2";
 import Step3 from "./_components/step3";
@@ -13,10 +13,10 @@ export default function Trait() {
   const methods = useForm({
     defaultValues: { mood: "", criterion: "", content: "" },
   });
-  const { formState } = methods;
 
   const handleClick = () => {
     setStep((prev) => prev + 1);
+    window.scrollTo(0, 0);
   };
 
   const onSubmit = methods.handleSubmit((data) => {
@@ -34,25 +34,35 @@ export default function Trait() {
 
   const nickname = "규빈";
 
+  const handleBack = () => {
+    if (step >= 1) {
+      setStep(step - 1);
+    }
+    console.log("dk");
+  };
+
   return (
     <FormProvider {...methods}>
       <form
         onSubmit={onSubmit}
-        className="relative mx-5 flex h-full flex-col items-center"
+        className="relative mx-5 flex flex-col items-center"
       >
-        {step === 0 && <Step0 nickname={nickname} />}
-        {step === 1 && <Step1 nickname={nickname} />}
-        {step === 2 && <Step2 />}
-        {step === 3 && <Step3 nickname={nickname} />}
-
-        <Button
-          type={step === 3 ? "submit" : "button"}
-          className="absolute bottom-19"
-          onClick={step < 3 ? handleClick : undefined}
-          disabled={isButtonDisabled}
-        >
-          {step === 0 ? " 시작하기" : "다음"}
-        </Button>
+        <Header onBack={handleBack} className="bg-gray-black fixed w-full" />
+        <div className="mt-12.5 mb-38 w-full flex-grow overflow-y-auto">
+          {step === 0 && <Step0 nickname={nickname} />}
+          {step === 1 && <Step1 nickname={nickname} />}
+          {step === 2 && <Step2 />}
+          {step === 3 && <Step3 nickname={nickname} />}
+        </div>
+        <div className="bg-gray-black fixed bottom-0 w-full max-w-125 px-5 pt-5 pb-19">
+          <Button
+            type={step === 3 ? "submit" : "button"}
+            onClick={step < 3 ? handleClick : undefined}
+            disabled={isButtonDisabled}
+          >
+            {step === 0 ? " 시작하기" : "다음"}
+          </Button>
+        </div>
       </form>
     </FormProvider>
   );
