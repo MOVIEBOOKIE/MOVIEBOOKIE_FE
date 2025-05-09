2d3a866c6b85bfcd951df6cb150ae008372bcb21
Sohyunnnn <thgus05061@naver.com>
2025-05-09 13:44:38 +0900

    style: 아이콘 위치 수정

--- src/app/(fullscreen)/trait/_components/step0.tsx
+++ src/app/(fullscreen)/trait/_components/step0.tsx
@@ -12,20 +12,24 @@ interface Step0Props {
 
 export default function Step0({ nickname }: Step0Props) {
   return (
-    <div className="relative w-93.75">
-      <TelevisionIcon className="absolute top-27.5 left-27.5 rotate-13" />
-      <SmileIcon className="absolute top-29.25 left-53" />
-      <PopcornIcon className="absolute top-48.5 left-23 -rotate-15" />
-      <BallIcon className="absolute top-42.5 left-41" />
-      <CamcorderIcon className="absolute top-49.5 left-56.5" />
-      <p className="title-1-bold text-gray-white absolute top-88 w-full text-center">
-        {nickname}님을 위해 <br />
-        간단한 테스트를 준비했어요
-      </p>
-      <p className="caption-1-medium absolute top-110.5 w-full text-center text-gray-500">
-        테스트 후 규빈님께 꼭 맞는 이벤트들을 추천해드려요.
-        <br /> 지금 바로 3가지 유형 테스트를 시작해볼까요?
-      </p>
+    <div className="relative overflow-hidden">
+      <div className="absolute left-1/2 h-full w-93.75 -translate-x-1/2">
+        <TelevisionIcon className="absolute top-27.5 left-27.5 rotate-13" />
+        <SmileIcon className="absolute top-29.25 left-53" />
+        <PopcornIcon className="absolute top-48.5 left-23 -rotate-15" />
+        <BallIcon className="absolute top-42.5 left-41" />
+        <CamcorderIcon className="absolute top-49.5 left-56.5" />
+      </div>
+      <div className="mt-80 w-full text-center">
+        <p className="title-1-bold text-gray-white">
+          {nickname}님을 위해 <br />
+          간단한 테스트를 준비했어요
+        </p>
+        <p className="caption-1-medium mt-6 text-gray-500">
+          테스트 후 {nickname}님께 꼭 맞는 이벤트들을 추천해드려요.
+          <br /> 지금 바로 3가지 유형 테스트를 시작해볼까요?
+        </p>
+      </div>
     </div>
   );
 }
