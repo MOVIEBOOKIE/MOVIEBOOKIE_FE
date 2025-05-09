b9e329165034d02b3f8c88a5330e8830c4386812
Sohyunnnn <thgus05061@naver.com>
2025-05-09 19:46:41 +0900

    style: asset 및 intro 스타일 수정

--- src/app/(fullscreen)/intro/page.tsx
+++ src/app/(fullscreen)/intro/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { LogoWhiteIcon } from "@/icons/index";
+
 //TODO: setTimeout 3초
-import Logo from "@/icons/logo-white.svg";
 export default function Intro() {
   return (
     <main className="relative h-screen w-full bg-[url('/images/custom-bg.png')] bg-cover bg-center">
-      <Logo className="absolute top-[330px] left-1/2 -translate-x-1/2" />
+      <LogoWhiteIcon
+        width={100}
+        height={100}
+        className="absolute top-[330px] left-1/2 -translate-x-1/2"
+      />
       <h1 className="body-2-medium absolute top-[419px] left-1/2 -translate-x-1/2 text-white">
         당신의 일상을 영화처럼
       </h1>
