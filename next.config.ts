4eaa92c6a0220c6e5252a515e3442eee445fa6c0
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-10 21:53:58 +0900

    fix: build error 수정

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
+const path = require("path");
 const withPWA = require("next-pwa")({
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
 });
 
 const withSvgr = require("next-svgr");
 
-const nextConfig = { trailingSlash: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  trailingSlash: true,
+  appDir: true,
+  outputFileTracingRoot: path.resolve(__dirname),
+};
 
 module.exports = withSvgr(withPWA(nextConfig));
