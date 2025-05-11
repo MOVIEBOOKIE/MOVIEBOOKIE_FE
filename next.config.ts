64be0f59536f766b72fa194ec868b03106250cda
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-11 17:33:15 +0900

    fix: 배포환경 png깨지는 문제

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
+  buildExcludes: [/middleware-manifest.json$/],
 });
 
 const withSvgr = require("next-svgr");
@@ -16,6 +17,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === "production" ? "" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "" : "",
 };
 
 module.exports = withSvgr(withPWA(nextConfig));
