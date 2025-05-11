b1d415e957151b0f6e413318aebc367778caf157
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-11 17:47:42 +0900

    fix: category 변수명수정

--- src/app/(fullscreen)/category/[category]/page.tsx
+++ src/app/(fullscreen)/category/[category]/page.tsx
@@ -1,23 +1,30 @@
-import { use } from "react";
 import { CATEGORY_TYPE_TO_LABEL, CATEGORIES } from "@/constants/categories";
 import { notFound } from "next/navigation";
 import { MOVIE_LISTS } from "@/mocks/movie-list";
 import CategoryPageClient from "./client";
 
-export const dynamic = "force-static"; // 정적 페이지 강제 생성
-
+export const dynamic = "force-static";
 export async function generateStaticParams() {
-  const categories = ["movies", "dramas", "shows", "concerts"]; // 가능한 카테고리 목록
+  const categories = [
+    "movie",
+    "drama",
+    "entertainment",
+    "sports",
+    "concert",
+    "others",
+  ];
 
   return categories.map((category) => ({
     category,
   }));
 }
 
-export default function CategoryPage(props: {
-  params: Promise<{ category: string }>;
+export default function CategoryPage({
+  params,
+}: {
+  params: { category: string };
 }) {
-  const { category } = use(props.params);
+  const { category } = params;
 
   const label =
     CATEGORY_TYPE_TO_LABEL[category as keyof typeof CATEGORY_TYPE_TO_LABEL];
