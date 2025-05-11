49113346b7481ffe27baebd236f02f8796434237
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-12 02:28:50 +0900

    feat: 코드복구, css 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,12 @@
 import { ReactNode } from "react";
 import "@/styles/globals.css";
 import { pretendard } from "../app/fonts/pretendard";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "무비부키",
-  description: "당신의 일상을 영화처럼",
-  themeColor: "#0E0E0E",
-  manifest: "/manifest.json",
-};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko" className={pretendard.variable}>
       <head>
         <link rel="icon" href="/images/favicon.png" sizes="512x512" />
-        <link rel="manifest" href="/manifest.json" />
       </head>
       <body>{children}</body>
     </html>
