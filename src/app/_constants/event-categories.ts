2a47e5d1954ae6cd41a8b63c24c5c2d7616028c0
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-13 02:59:18 +0900

    fix: 폴더 분리 후  IMAGE_PATH 적용

--- src/app/_constants/event-categories.ts
+++ src/app/_constants/event-categories.ts
@@ -1,31 +1,25 @@
+import { PATH_IMAGES } from "./path-images";
 import { PATHS } from "./paths";
-
-export const IMAGE_PATHS = {
-  POPULAR: "/images/popular.png",
-  RECENT: "/images/recent.png",
-  DRAMA: "/images/drama.png",
-  MOVIE: "/images/movie.png",
-};
 
 export const EVENT_CATEGORIES = [
   {
     label: "인기 이벤트",
-    src: IMAGE_PATHS.POPULAR,
+    src: PATH_IMAGES.POPULAR,
     path: PATHS.CATEGORY.POPULAR,
   },
   {
     label: "최근 이벤트",
-    src: IMAGE_PATHS.RECENT,
+    src: PATH_IMAGES.RECENT,
     path: PATHS.CATEGORY.RECENT,
   },
   {
     label: "드라마 최종회\n같이 보기",
-    src: IMAGE_PATHS.DRAMA,
+    src: PATH_IMAGES.DRAMA,
     path: PATHS.CATEGORY.DRAMA,
   },
   {
     label: "명작 영화\n다시보기",
-    src: IMAGE_PATHS.MOVIE,
+    src: PATH_IMAGES.MOVIE,
     path: PATHS.CATEGORY.MOVIE,
   },
 ];
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from "react";
 import "@/styles/globals.css";
 import { pretendard } from "../app/fonts/pretendard";
+import { PATH_IMAGES } from "./_constants";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko" className={pretendard.variable}>
       <head>
-        <link rel="icon" href="/images/favicon.png" sizes="512x512" />
+        <link rel="icon" href={PATH_IMAGES.FAVICON} sizes="512x512" />
       </head>
       <body>{children}</body>
     </html>
