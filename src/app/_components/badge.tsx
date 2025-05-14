d6e247659a91501b40b048ae871827f5b7aad5dc
Sohyunnnn <thgus05061@naver.com>
2025-05-15 02:21:00 +0900

    refactor: 뱃지 variant 추가

--- src/app/_components/badge.tsx
+++ src/app/_components/badge.tsx
@@ -3,14 +3,24 @@ import { cn } from "../_utils/cn";
 interface BadgeProps {
   children: string;
   className?: string;
+  variant?: "primary" | "secondary";
 }
 
-export default function Badge({ children, className }: BadgeProps) {
+export default function Badge({
+  children,
+  className,
+  variant = "primary",
+}: BadgeProps) {
+  const badgeStyle =
+    variant === "secondary"
+      ? "bg-gray-950 text-gray-300"
+      : "bg-red-main text-white";
   return (
     <div
       className={cn(
-        "caption-1-medium rounded-md bg-gray-950 px-1.75 py-1 text-gray-300",
+        "caption-1-medium rounded-md px-1.75 py-1 text-gray-300",
         className,
+        badgeStyle,
       )}
     >
       {children}
