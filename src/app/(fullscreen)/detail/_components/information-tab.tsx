563c5bec9dca6e44a957a1b70cef869d55308391
Sohyunnnn <thgus05061@naver.com>
2025-05-15 02:58:16 +0900

    feat: 모집정보탭 퍼블리싱

--- src/app/(fullscreen)/detail/_components/information-tab.tsx
+++ src/app/(fullscreen)/detail/_components/information-tab.tsx
@@ -19,7 +19,7 @@ export default function InformationTab() {
       {selected === "이벤트" ? (
         <div className="mt-5 pb-35">
           <p className="body-2-medium text-gray-400">가격</p>
-          <p className="caption-1-medium mt-0.5">
+          <p className="caption-1-medium mt-0.5 text-gray-600">
             *최소 인원 기준 가격으로, 모집인원에 따라 변동가능
           </p>
           <div className="mt-2.5 flex items-center gap-2">
@@ -49,7 +49,31 @@ export default function InformationTab() {
           />
         </div>
       ) : (
-        <div></div>
+        <div className="mt-5 pb-17">
+          <p className="body-2-medium mb-2 text-gray-400">모집 일정</p>
+          <div className="flex gap-3">
+            <p className="body-3-medium text-gray-400">모집 기간</p>
+            <div className="flex gap-1.5">
+              <p className="body-3-regular text-gray-200">
+                2025. 04. 15 - 2025. 04. 20
+              </p>
+              <Badge variant="primary">D-4</Badge>
+            </div>
+          </div>
+          <div className="mt-6 mb-5 h-0.25 w-full rounded-sm bg-gray-950" />
+          <p className="body-2-medium mb-2 text-gray-400">모집 일정</p>
+          <p className="caption-1-medium mt-0.5 text-gray-600">
+            *모집 인원 미달성시, 이벤트 진행 취소
+          </p>
+          <div className="mt-2.5 grid grid-cols-[auto_1fr] gap-x-3 gap-y-1.5">
+            <span className="body-3-medium text-gray-600">모집 인원</span>
+            <p className="body-3-regular text-gray-200">20 - 56명</p>
+            <span className="body-3-medium text-gray-600">현재 참여자</span>
+            <p className="body-3-regular text-gray-200">12명 신청</p>
+            <span className="body-3-medium text-gray-600">모집 달성률</span>
+            <p className="body-3-medium text-red-main">60%</p>
+          </div>
+        </div>
       )}
     </div>
   );
