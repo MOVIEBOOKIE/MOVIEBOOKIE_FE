128b86cf815d9b889b30301ea77470fae1b86956
HYERYEON <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-14 12:47:20 +0900

    [Bug/#55] 카카오 로그인 에러, 빌드 에러 (#56)
    * fix: 카카오 로그인 에러
    
    * fix: env 일치 시키기

--- src/app/api/auth/auth.ts
+++ src/app/api/auth/auth.ts
@@ -1,15 +1,13 @@
 import { apiRequest } from "@/utils/api";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_DEV_URL;
-
 export const sendAuthCodeToServer = async (
   code: string,
-  redirectUrl: string,
+  redirectUri: string,
   isLocal: boolean,
 ) => {
-  const requestUrl = `${API_BASE_URL}/api/auth/login/kakao?code=${encodeURIComponent(
+  const requestUrl = `/api/auth/login/kakao?code=${encodeURIComponent(
     code,
-  )}&redirectUrl=${encodeURIComponent(redirectUrl)}&isLocal=${isLocal}`;
+  )}&redirectUri=${encodeURIComponent(redirectUri)}&isLocal=${isLocal}`;
 
   return apiRequest(requestUrl);
 };
