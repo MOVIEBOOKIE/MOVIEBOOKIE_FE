e522bf1696e64cddb2966adbb18bd3cc133fda6a
Sohyunnnn <thgus05061@naver.com>
2025-05-16 16:57:56 +0900

    style: 패딩값 조절

--- src/app/_components/fixed-layout.tsx
+++ src/app/_components/fixed-layout.tsx
@@ -49,7 +49,7 @@ export default function FixedLayout({
       <div className={`${paddingStyle} flex min-h-screen flex-col text-white`}>
         <div className="flex-1 pb-30">{children}</div>
       </div>
-      <div className="bg-gray-black fixed bottom-0 z-50 w-full max-w-125 px-5 pt-2 pb-19">
+      <div className="bg-gray-black fixed bottom-0 z-50 w-full max-w-125 px-5 pt-5 pb-12.5">
         <Button
           disabled={isButtonDisabled}
           onClick={onButtonClick}
