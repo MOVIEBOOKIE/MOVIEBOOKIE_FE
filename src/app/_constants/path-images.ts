6cc4f923ece954eeae4d122197d68fe97f803d33
Sohyunnnn <thgus05061@naver.com>
2025-05-16 16:45:51 +0900

    refactor: mock image url 상수화

--- src/app/_constants/path-images.ts
+++ src/app/_constants/path-images.ts
@@ -14,3 +14,24 @@ export const PATH_IMAGES = {
   TMP_MAP: "/images/tmp-map.png",
   TMP_MOVIE: "/images/tmp-movie.png",
 };
+
+export const MOCK_IMAGES = {
+  IMAGE_1: "/mock_images/mock-image-1.png",
+  IMAGE_2: "/mock_images/mock-image-2.png",
+  IMAGE_3: "/mock_images/mock-image-3.png",
+  IMAGE_4: "/mock_images/mock-image-4.png",
+  IMAGE_5: "/mock_images/mock-image-5.png",
+  IMAGE_6: "/mock_images/mock-image-6.png",
+  IMAGE_7: "/mock_images/mock-image-7.png",
+  IMAGE_8: "/mock_images/mock-image-8.png",
+  IMAGE_9: "/mock_images/mock-image-9.png",
+  IMAGE_10: "/mock_images/mock-image-10.png",
+  IMAGE_11: "/mock_images/mock-image-11.png",
+  IMAGE_12: "/mock_images/mock-image-12.png",
+  IMAGE_13: "/mock_images/mock-image-13.png",
+  IMAGE_14: "/mock_images/mock-image-14.png",
+  IMAGE_15: "/mock_images/mock-image-15.png",
+  IMAGE_16: "/mock_images/mock-image-16.png",
+  IMAGE_17: "/mock_images/mock-image-17.png",
+  IMAGE_18: "/mock_images/mock-image-18.png",
+};
--- src/app/_mocks/movie-slides.ts
+++ src/app/_mocks/movie-slides.ts
@@ -1,39 +1,34 @@
-import mock_image_2 from "../../../public/mock_imges/mock-image-2.png";
-import mock_image_3 from "../../../public/mock_imges/mock-image-3.png";
-import mock_image_4 from "../../../public/mock_imges/mock-image-4.png";
-import mock_image_5 from "../../../public/mock_imges/mock-image-5.png";
-import mock_image_10 from "../../../public/mock_imges/mock-image-10.png";
-import mock_image_11 from "../../../public/mock_imges/mock-image-11.png";
+import { MOCK_IMAGES } from "@/constants/path-images";
 
 export const MOVIE_SLIDES = [
   {
     id: 1,
     title: "솔로지옥4 마지막회",
-    image: mock_image_11,
+    image: MOCK_IMAGES.IMAGE_11,
   },
   {
     id: 2,
     title: "오징어게임3",
-    image: mock_image_4,
+    image: MOCK_IMAGES.IMAGE_4,
   },
   {
     id: 3,
     title: "극한직업",
-    image: mock_image_2,
+    image: MOCK_IMAGES.IMAGE_2,
   },
   {
     id: 4,
     title: "더 글로리",
-    image: mock_image_10,
+    image: MOCK_IMAGES.IMAGE_10,
   },
   {
     id: 5,
     title: "아이유 콘서트 : 더 위닝",
-    image: mock_image_5,
+    image: MOCK_IMAGES.IMAGE_5,
   },
   {
     id: 6,
     title: "환승연애2",
-    image: mock_image_3,
+    image: MOCK_IMAGES.IMAGE_3,
   },
 ];
