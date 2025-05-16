1f9bd1bb08000456c2e5e9d3ba30f01b60d5c723
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-17 01:37:08 +0900

    refactor: 코드정리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <InAppRedirect />
       <html lang="ko" className={pretendard.variable}>
         <head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-          />
           <link rel="manifest" href="/manifest.json" />
         </head>
         <body>{children}</body>
