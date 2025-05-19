c9a4a13d2d3d2478289ae69c11556f7980cb950e
Sohyun Park <108279922+Sohyunnnn@users.noreply.github.com>
2025-05-20 03:04:25 +0900

    [Feature/#95] 토스트, 공통모달 UI 구현 (#97)
    * rename: 이름 변경
    
    * style: 스타일 수정
    
    * style: 전역 스타일 추가
    
    * feat: 토스트 UI 구현
    
    * asset: add assets
    
    * style: 전역 스타일 추가
    
    * feat: 공통 모달 구현
    
    * style: 스타일 수정

---
+++ src/app/_components/modal.tsx
@@ -0,0 +1,51 @@
+import { AlertIcon, CheckIcon } from "@/icons/index";
+import Button from "./button";
+import { ReactNode } from "react";
+
+/**
+ * 공통 모달 컴포넌트
+ *
+ * @param iconType - 아이콘 타입. `"alert"` 또는 `"confirm"` 중 하나를 선택.
+ * @param title - 모달의 제목 텍스트
+ * @param description - 모달의 설명 또는 본문. ReactNode이므로 텍스트 외 `<span>`, `<br />` 등 추가 가능.
+ * @param confirmText - 확인 버튼에 표시될 텍스트.
+ * @param cancelText - 취소 버튼에 표시될 텍스트. 기본값: `"아니요"`
+ * @param onConfirm - 확인 버튼 클릭 시 실행할 콜백 함수.
+ * @param onCancel - 취소 버튼 클릭 시 실행할 콜백 함수.
+ */
+
+interface ModalProps {
+  iconType: "alert" | "confirm";
+  title: string;
+  description: ReactNode;
+  confirmText: string;
+  cancelText?: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
+
+export default function Modal({
+  iconType,
+  title,
+  description,
+  confirmText,
+  cancelText = "아니요",
+  onConfirm,
+  onCancel,
+}: ModalProps) {
+  return (
+    <div className="flex w-80 flex-col items-center rounded-2xl bg-gray-900 px-5 pt-6 pb-5 drop-shadow">
+      {iconType === "confirm" ? <CheckIcon /> : <AlertIcon />}
+      <h3 className="title-3-semibold mt-4.75">{title}</h3>
+      <p className="body-3-regular mt-0.5 mb-4.5 text-gray-500">
+        {description}
+      </p>
+      <div className="flex w-full gap-2.75">
+        <Button variant="secondary" onClick={onCancel}>
+          {cancelText}
+        </Button>
+        <Button onClick={onConfirm}>{confirmText}</Button>
+      </div>
+    </div>
+  );
+}
