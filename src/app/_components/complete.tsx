a79ea7d64014c50ccfe0d6b8963cf96ca5cfdd8c
Sohyunnnn <thgus05061@naver.com>
2025-05-20 01:10:56 +0900

    style: 스타일 수정

--- src/app/_components/complete.tsx
+++ src/app/_components/complete.tsx
@@ -10,7 +10,7 @@ export default function Complete({ state, buttonText }: CompleteProps) {
   return (
     <FixedLayout isHeader={false} buttonText={buttonText} state="full">
       <div className="flex h-[calc(100vh-126px)] flex-col items-center justify-center gap-3">
-        <CheckboxIcon className="mb-2" />
+        <CheckboxIcon width={45} height={45} className="mb-2" />
         <p className="title-1-bold text-gray-white">{state}이 완료됐어요!</p>
         <p className="body-3-regular text-center text-gray-500">
           모집 기간 내 인원 미달성시,
