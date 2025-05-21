bfd14a2a19a4a121105d348915bfc026354ac9e7
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-21 20:36:50 +0900

    feat: 캘린더 토스트 추가

--- src/app/_components/toast.tsx
+++ src/app/_components/toast.tsx
@@ -1,15 +1,18 @@
-import { CheckboxIcon } from "@/icons/index";
+import { CheckboxIcon, ToastAlertIcon } from "@/icons/index";
 import React from "react";
 
 interface ToastProps {
   children: string;
+  iconType?: "checkbox" | "alert";
 }
 
-export default function Toast({ children }: ToastProps) {
+export default function Toast({ children, iconType = "checkbox" }: ToastProps) {
+  const IconComponent = iconType === "alert" ? ToastAlertIcon : CheckboxIcon;
+
   return (
-    <div className="drop-shadow-2 flex w-88.75 items-center gap-2.5 rounded-[14px] bg-gray-900 p-3">
-      <CheckboxIcon width={26} height={26} className="m-0.5" />
-      <p className="body-3-medium">{children}</p>
+    <div className="drop-shadow-2 flex w-[335px] items-center gap-2.5 rounded-[14px] bg-gray-900 p-3">
+      <IconComponent width={30} height={30} className="m-0.5" />
+      <p className="body-3-medium text-white">{children}</p>
     </div>
   );
 }
--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -59,3 +59,4 @@ export { default as KissingFaceIcon } from "./ui/kissingface.svg";
 
 export { default as CheckIcon } from "./ui/check.svg";
 export { default as AlertIcon } from "./ui/alert.svg";
+export { default as ToastAlertIcon } from "./ui/toast-alert.svg";
