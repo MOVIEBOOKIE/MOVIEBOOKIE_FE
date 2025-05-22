3be282d722c81e27c862935ba4b42542b50431fe
Sohyunnnn <thgus05061@naver.com>
2025-05-22 13:54:16 +0900

    asset: add assets

--- src/app/_constants/path-images.ts
+++ src/app/_constants/path-images.ts
@@ -13,6 +13,19 @@ export const PATH_IMAGES = {
   IMAGE: "/images/image.png",
   TMP_MAP: "/images/tmp-map.png",
   TMP_MOVIE: "/images/tmp-movie.png",
+
+  TRAIT: {
+    MOVIE_DETAIL_COLLECTOR: "/images/trait/movie1.png",
+    MOVIE_SHARED_EMOTER: "/images/trait/movie2.png",
+    DRAMA_STORY_IMMERSER: "/images/trait/movie3.png",
+    DRAMA_SOCIAL_FAN: "/images/trait/movie4.png",
+    VARIETY_DETAIL_ANALYST: "/images/trait/movie5.png",
+    VARIETY_REACTION_SHARER: "/images/trait/movie6.png",
+    SPORTS_FULL_SUPPORTER: "/images/trait/movie7.png",
+    SPORTS_MOMENT_LOVER: "/images/trait/movie8.png",
+    CONCERT_STAGE_DIVER: "/images/trait/movie9.png",
+    CONCERT_FANCAM_LOVER: "/images/trait/movie10.png",
+  },
 };
 
 export const MOCK_IMAGES = {
