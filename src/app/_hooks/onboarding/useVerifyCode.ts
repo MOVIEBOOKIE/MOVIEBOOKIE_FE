88e9ce7f76a28387eac27d5da78dd51dce14deed
HYERYEON <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-23 05:05:42 +0900

    [Feature/101-2] 사용자인증 api, 인원제한, 캘린더 토스트 추가 (#128)
    * feat: 인원제한, 생성 close버튼 이벤트추가
    
    * fix: 카카오로그인 함수 수정
    
    * feat: 사용자인증 api 연결

---
+++ src/app/_hooks/onboarding/useVerifyCode.ts
@@ -0,0 +1,27 @@
+import { useMutation } from "@tanstack/react-query";
+import { apiPost } from "app/_apis/methods";
+interface VerifySmsPayload {
+  phoneNum: string;
+  certificationCode: string;
+}
+
+export const useVerifySms = () => {
+  return useMutation({
+    mutationFn: async (payload: VerifySmsPayload) => {
+      return await apiPost<null, VerifySmsPayload>("/sms/verify", payload);
+    },
+  });
+};
+
+interface VerifyEmailPayload {
+  email: string;
+  certificationCode: string;
+}
+
+export const useVerifyEmail = () => {
+  return useMutation({
+    mutationFn: async (payload: VerifyEmailPayload) => {
+      return await apiPost<null, VerifyEmailPayload>("/email/verify", payload);
+    },
+  });
+};
