6edaf92786350499aedc97a8a0248cf62412fa45
Sohyun Park <108279922+Sohyunnnn@users.noreply.github.com>
2025-05-22 23:20:05 +0900

    fix: 컴포넌트 분리 (#119)

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,21 +1,5 @@
-"use client";
-
-import Lottie from "lottie-react";
-import loadingAnimation from "@/lotties/loading.json";
+import LottieClient from "./_components/lottie";
 
 export default function Loading() {
-  return (
-    <div className="relative flex h-screen w-full justify-center">
-      <div
-        className="relative inline-flex flex-col"
-        style={{ paddingTop: "30vh" }}
-      >
-        <Lottie animationData={loadingAnimation} loop autoplay />
-        <p className="body-1-medium z-1 -mt-15.5 text-center text-gray-200">
-          무비부키가 열심히
-          <br /> 정보를 가져오고 있어요
-        </p>
-      </div>
-    </div>
-  );
+  return <LottieClient />;
 }
