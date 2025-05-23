baa94f26f83a4b5571f78f4a344ef600aa074721
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-23 23:13:23 +0900

    refactor: 검색, 검색버튼 디자인 qa 반영

--- src/app/_components/input.tsx
+++ src/app/_components/input.tsx
@@ -1,29 +1,37 @@
 import { cn } from "@/utils/cn";
 import { SearchIcon } from "@/icons/index";
+import { forwardRef } from "react";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   type: "BUTTON" | "INPUT";
   className?: string;
 }
 
-export default function Input({ type, className, ...props }: InputProps) {
-  const inputClass = cn(
-    "body-2-medium w-full h-full placeholder:text-gray-800 pl-4 text-gray-800 text-start focus:outline-none",
-    type === "INPUT" ? "focus:text-gray-100" : "",
-    className,
-  );
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ type, className, ...props }, ref) => {
+    const inputClass = cn(
+      "body-2-medium w-full h-full placeholder:text-gray-800 pl-4 text-gray-800 text-start focus:outline-none",
+      type === "INPUT" ? "focus:text-gray-100" : "",
+      className,
+    );
 
-  return (
-    <div className="flex h-13 w-full items-center gap-2 rounded-xl bg-gray-950 pr-3">
-      <input
-        className={inputClass}
-        {...props}
-        type={type === "BUTTON" ? "button" : "text"}
-        {...(type === "BUTTON"
-          ? { value: "이벤트 검색하기" }
-          : { placeholder: "이벤트 검색하기" })}
-      />
-      <SearchIcon width={24} height={24} />
-    </div>
-  );
-}
+    return (
+      <div className="flex h-13 w-full items-center gap-2 rounded-xl bg-gray-950 pr-3">
+        <input
+          ref={ref}
+          className={inputClass}
+          type={type === "BUTTON" ? "button" : "text"}
+          {...(type === "BUTTON"
+            ? { value: "이벤트 검색하기" }
+            : { placeholder: "이벤트 검색하기" })}
+          {...props}
+        />
+        <SearchIcon width={24} height={24} />
+      </div>
+    );
+  },
+);
+
+Input.displayName = "Input";
+
+export default Input;
