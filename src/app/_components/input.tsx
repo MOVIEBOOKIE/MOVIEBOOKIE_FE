73be6443a390fba0d35f8d1d1a28470f84035c37
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-23 20:26:04 +0900

    fix: 검색 컴포넌트 css 수정

--- src/app/_components/input.tsx
+++ src/app/_components/input.tsx
@@ -8,13 +8,13 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 
 export default function Input({ type, className, ...props }: InputProps) {
   const inputClass = cn(
-    "body-2-medium w-full h-full placeholder:text-gray-800 pl-4 text-gray-800 text-start focus:outline-none",
+    "body-2-medium w-full h-full placeholder:text-gray-700 pl-4 text-gray-700 text-start focus:outline-none",
     type === "INPUT" ? "focus:text-gray-100" : "",
     className,
   );
 
   return (
-    <div className="flex h-13 w-full items-center gap-2 rounded-xl bg-gray-950 pr-3">
+    <div className="flex h-13 w-full items-center gap-2 rounded-xl bg-gray-900 pr-3 text-gray-700">
       <input
         className={inputClass}
         {...props}
