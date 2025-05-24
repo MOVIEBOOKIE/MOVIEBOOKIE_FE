d29d91fa05ec824381bb77b0e68d4686e64c3822
HYERYEON <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-24 14:59:09 +0900

    [Feature/#131] 이벤트탭: 만든이벤트 조회 API 연동 (#132)
    * feat: 디자인 qa 생성탭 반영
    
    * feat: 이벤트탭 -신청목록, 내이벤트 api연결
    
    * fix: 디자인 qa 반영
    
    * fix: 검색 컴포넌트 css 수정
    
    * fix: 빌드에러 해결
    
    * feat: 매핑함수로 변수명 통일, 빌드오류 해결
    
    * fix: 타입 구조 통일, 매핑함수 사용

--- src/app/_apis/methods.ts
+++ src/app/_apis/methods.ts
@@ -1,42 +1,42 @@
 import axiosInstance from "./instance";
 import { ApiResponse } from "./type";
 
-export const apiGet = async <T, P = undefined>(
+export const apiGet = async <T, P = Record<string, any>>(
   url: string,
   params?: P,
 ): Promise<T> => {
   const response = await axiosInstance.get<ApiResponse<T>>(url, { params });
-  return response.data.result;
+  return response.data.data;
 };
 
-export const apiPost = async <T, B = undefined>(
+export const apiPost = async <T, B = Record<string, any>>(
   url: string,
   body?: B,
 ): Promise<T> => {
   const response = await axiosInstance.post<ApiResponse<T>>(url, body);
-  return response.data.result;
+  return response.data.data;
 };
 
-export const apiPatch = async <T, B = undefined>(
+export const apiPatch = async <T, B = Record<string, any>>(
   url: string,
   body?: B,
 ): Promise<T> => {
   const response = await axiosInstance.patch<ApiResponse<T>>(url, body);
-  return response.data.result;
+  return response.data.data;
 };
 
-export const apiPut = async <T, B = undefined>(
+export const apiPut = async <T, B = Record<string, any>>(
   url: string,
   body?: B,
 ): Promise<T> => {
   const response = await axiosInstance.put<ApiResponse<T>>(url, body);
-  return response.data.result;
+  return response.data.data;
 };
 
-export const apiDelete = async <T, P = undefined>(
+export const apiDelete = async <T, P = Record<string, any>>(
   url: string,
   params?: P,
 ): Promise<T> => {
   const response = await axiosInstance.delete<ApiResponse<T>>(url, { params });
-  return response.data.result;
+  return response.data.data;
 };
