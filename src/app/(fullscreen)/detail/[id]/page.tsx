0a04a7ea2ed5c8ecd32b4c5b510935430a12784c
Sohyunnnn <thgus05061@naver.com>
2025-05-26 01:57:02 +0900

    feat: 상태별 모달 추가

--- src/app/(fullscreen)/detail/[id]/page.tsx
+++ src/app/(fullscreen)/detail/[id]/page.tsx
@@ -5,14 +5,56 @@ import TopBar from "../_components/top-bar";
 import DetailContent from "@/components/detail-content";
 import { useParams } from "next/navigation";
 import { useGetEvent } from "app/_hooks/events/use-events";
+import { useState } from "react";
+import Modal from "@/components/modal";
+
+type ModalType = "apply" | "cancel" | "recruitCancel" | null;
 
 export default function Detail() {
+  const [modalType, setModalType] = useState<ModalType>(null);
   const params = useParams();
   const id = params?.id;
-
   const eventId = Number(id);
 
   const { data } = useGetEvent(eventId);
+
+  const handleClick = () => {
+    switch (data?.buttonState) {
+      case "신청하기":
+        setModalType("apply");
+        break;
+      case "신청 취소":
+        setModalType("cancel");
+        break;
+      case "모집 취소":
+        setModalType("recruitCancel");
+        break;
+    }
+  };
+
+  const modalContent = {
+    apply: {
+      iconType: "confirm",
+      title: "이벤트를 신청할까요?",
+      description: "이벤트 정보를 자세히 확인한 후 신청해주세요",
+      confirmText: "신청하기",
+    },
+    cancel: {
+      iconType: "alert",
+      title: "이벤트 신청을 취소할까요?",
+      description: "지금 취소해도 다시 신청할 수 있어요",
+      confirmText: "신청 취소",
+    },
+    recruitCancel: {
+      iconType: "alert",
+      title: "정말 이벤트 모집을 취소할까요?",
+      description: "지금 취소하면 이벤트가 완전히 삭제돼요",
+      confirmText: "모집 취소",
+    },
+  };
+
+  const currentModal = modalType ? modalContent[modalType] : null;
+
   return (
     <>
       <TopBar />
@@ -26,8 +68,21 @@ export default function Detail() {
             </p>
           )}
         </div>
-        <Button variant="primary">{data?.buttonState}</Button>
+        <Button variant="primary" onClick={handleClick}>
+          {data?.buttonState}
+        </Button>
       </div>
+
+      {currentModal && (
+        <Modal
+          iconType={currentModal.iconType as "confirm" | "alert"}
+          title={currentModal.title}
+          description={currentModal.description}
+          confirmText={currentModal.confirmText}
+          onConfirm={() => setModalType(null)}
+          onCancel={() => setModalType(null)}
+        />
+      )}
     </>
   );
 }
