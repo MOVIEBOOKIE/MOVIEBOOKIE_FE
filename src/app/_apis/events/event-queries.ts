359d1b523729e3a0c8eacbc66a37af069b321d99
Sohyunnnn <thgus05061@naver.com>
2025-05-25 23:45:12 +0900

    style: 스타일 수정

--- src/app/_apis/events/event-queries.ts
+++ src/app/_apis/events/event-queries.ts
@@ -2,13 +2,13 @@ import { queryOptions } from "@tanstack/react-query";
 import { getEvents } from "./events";
 
 export const EVENT_KEY = {
-  EVENT: () => ["event"],
+  EVENT: (eventId: number) => ["event", eventId],
 } as const;
 
 export const EVENT_OPTION = {
   EVENT: (eventId: number) =>
     queryOptions({
-      queryKey: EVENT_KEY.EVENT(),
+      queryKey: EVENT_KEY.EVENT(eventId),
       queryFn: () => getEvents(eventId),
     }),
 };
