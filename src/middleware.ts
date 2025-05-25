52e6fd4bd055be4e719dee88ebfb150eecdeeb9c
Sohyunnnn <thgus05061@naver.com>
2025-05-25 11:55:38 +0900

    fix: 리디렉션 오류 해결

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,15 @@ import { PATHS } from "@/constants";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+export const config = {
+  matcher: ["/", "/login"],
+};
+
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("accessToken")?.value;
+  const pathname = req.nextUrl.pathname;
 
-  if (!token) {
+  if (!token && pathname === "/") {
     return NextResponse.redirect(new URL(PATHS.LOGIN, req.url));
   }
 
