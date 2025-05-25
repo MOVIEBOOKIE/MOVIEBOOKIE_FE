a305fb21d4d7c7869b1867748df280da5477e4a0
Sohyunnnn <thgus05061@naver.com>
2025-05-26 02:11:34 +0900

    feat: 이벤트 생성 성공 페이지 추가

--- src/app/(fullscreen)/event/success/_components/client.tsx
+++ src/app/(fullscreen)/event/success/_components/client.tsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import { useEventFormStore } from "app/_stores/useEventCreateForm";
 import { PATHS } from "@/constants";
 import { useCreateEvent } from "app/_hooks/use-create-event";
+import Complete from "@/components/complete";
 
 export default function Client() {
   const [step, setStep] = useState(1);
@@ -39,6 +40,9 @@ export default function Client() {
     >
       {step === 1 && <Step1 />}
       {step === 2 && <Step2 />}
+      {step === 3 && (
+        <Complete state="이벤트 생성" buttonText="모집목록 확인하기" />
+      )}
     </FixedLayout>
   );
 }
