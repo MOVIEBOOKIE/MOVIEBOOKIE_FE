48bc919dcf65bd6e73cf0ca178033fdfc0add88f
Sohyunnnn <thgus05061@naver.com>
2025-05-27 02:54:22 +0900

    feat: iconType 추가

--- src/app/(fullscreen)/detail/_components/alert-content.tsx
+++ src/app/(fullscreen)/detail/_components/alert-content.tsx
@@ -1,13 +1,18 @@
-import { DangerIcon } from "@/icons/index";
+import { CheckAlertIcon, DangerIcon } from "@/icons/index";
 
 export interface AlertContentProps {
   children: string;
+  iconType: "danger" | "check";
 }
 
-export default function AlertContent({ children }: AlertContentProps) {
+export default function AlertContent({
+  children,
+  iconType,
+}: AlertContentProps) {
   return (
     <div className="mt-3 flex items-center justify-center gap-0.5 rounded-[10px] border-[1.4px] border-gray-900 py-2.5">
-      <DangerIcon />
+      {iconType === "danger" ? <DangerIcon /> : <CheckAlertIcon />}
+
       <p className="caption-1-medium text-gray-500">{children}</p>
     </div>
   );
