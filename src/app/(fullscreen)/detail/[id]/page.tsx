e5c5a656e3df91d2dcffabac9c90d14aba9fc824
Sohyunnnn <thgus05061@naver.com>
2025-05-26 11:07:53 +0900

    refactor: 상수 분리

--- src/app/(fullscreen)/detail/[id]/page.tsx
+++ src/app/(fullscreen)/detail/[id]/page.tsx
@@ -13,6 +13,7 @@ import {
 import { useState } from "react";
 import Modal from "@/components/modal";
 import Complete from "@/components/complete";
+import { MODAL_CONTENT } from "app/(fullscreen)/detail/_constants/detail";
 
 type ModalType = "apply" | "cancel" | "recruitCancel" | null;
 
@@ -39,28 +40,7 @@ export default function Detail() {
     }
   };
 
-  const modalContent = {
-    apply: {
-      iconType: "confirm",
-      title: "이벤트를 신청할까요?",
-      description: "이벤트 정보를 자세히 확인한 후 신청해주세요",
-      confirmText: "신청하기",
-    },
-    cancel: {
-      iconType: "alert",
-      title: "이벤트 신청을 취소할까요?",
-      description: "지금 취소해도 다시 신청할 수 있어요",
-      confirmText: "신청 취소",
-    },
-    recruitCancel: {
-      iconType: "alert",
-      title: "정말 이벤트 모집을 취소할까요?",
-      description: "지금 취소하면 이벤트가 완전히 삭제돼요",
-      confirmText: "모집 취소",
-    },
-  };
-
-  const currentModal = modalType ? modalContent[modalType] : null;
+  const currentModal = modalType ? MODAL_CONTENT[modalType] : null;
 
   const { mutate } = usePostEventRegister();
   const { mutate: applyCancel } = useDeleteEvent();
