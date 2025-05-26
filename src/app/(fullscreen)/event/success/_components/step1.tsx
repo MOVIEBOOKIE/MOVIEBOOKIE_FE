b4122a678b6f8d1230d0829adef28a96f8791713
Sohyunnnn <thgus05061@naver.com>
2025-05-26 15:49:10 +0900

    feat: 썸네일 사진 추가

--- src/app/(fullscreen)/event/success/_components/step1.tsx
+++ src/app/(fullscreen)/event/success/_components/step1.tsx
@@ -1,21 +1,38 @@
-import { PATH_IMAGES } from "@/constants";
+"use client";
+
+import { useEventFormStore } from "app/_stores/useEventCreateForm";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Step1() {
+  const { formData } = useEventFormStore();
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (formData.thumbnail) {
+      const url = URL.createObjectURL(formData.thumbnail);
+      setPreviewUrl(url);
+
+      return () => URL.revokeObjectURL(url);
+    }
+  }, [formData.thumbnail]);
+
   return (
     <>
       <div
-        className="relative flex w-full justify-center"
+        className="relative mt-11.5 flex w-full justify-center"
         style={{ aspectRatio: "375 / 302" }}
       >
         <div className="from-gray-black/0 to-gray-black absolute inset-0 z-10 bg-gradient-to-b" />
         <div className="to-70.53% from-gray-black absolute inset-0 z-10 bg-gradient-to-b from-0% to-black/0" />
-        <Image
-          src={PATH_IMAGES.IMAGE}
-          alt="movie-poster"
-          fill
-          className="object-cover"
-        />
+        {previewUrl ? (
+          <Image
+            src={previewUrl}
+            alt="movie-poster"
+            fill
+            className="object-cover"
+          />
+        ) : null}
       </div>
       <div className="mt-11.75 flex w-full flex-col justify-center gap-2">
         <p className="text-gray-white title-1-bold text-center">
