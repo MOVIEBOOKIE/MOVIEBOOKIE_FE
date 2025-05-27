716c38d6475262fae2397179fe6a4683adf44742
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-27 13:49:00 +0900

    feat: middleware 재추가

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { PATHS } from "@/constants";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export const config = {
+  matcher: ["/", "/login"],
+};
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("accessToken")?.value;
+  const pathname = req.nextUrl.pathname;
+
+  if (!token && pathname === "/") {
+    return NextResponse.redirect(new URL(PATHS.LOGIN, req.url));
+  }
+
+  return NextResponse.next();
+}
