869f76f5f508a903a76777f112924bae70d7aac2
Sohyunnnn <thgus05061@naver.com>
2025-05-29 01:31:19 +0900

    fix: 상태관리 오류 해결

--- src/app/(fullscreen)/event-create/components/step7-writing.tsx
+++ src/app/(fullscreen)/event-create/components/step7-writing.tsx
@@ -99,7 +99,7 @@ export default function Step7() {
             콘텐츠 제목
           </label>
           <input
-            {...register("eventTitle")}
+            {...register("mediaTitle")}
             type="text"
             placeholder="콘텐츠의 제목을 입력해 주세요 (ex 타이타닉)"
             className="body-3-medium w-full rounded-xl border border-gray-900 p-5 text-gray-300 placeholder-gray-800 focus:outline-none"
