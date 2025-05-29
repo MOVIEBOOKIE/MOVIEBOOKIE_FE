f915b6baccda467b255fac2716310b3cbd314385
HYERYEON <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-29 11:29:25 +0900

    [Fix/qa] 이벤트 토글 무한스크롤, 스켈레톤 로딩 처리  (#194)
    * fix: 이벤트 신청목록 무한스크롤
    
    * feat: 이벤트탭 토글 이동 스켈레톤 적용
    
    * feat: 신규 가입자 구분해서 라우팅
    
    * fix: 로그인 랜딩 1번 css 애니메이션
    
    * fix: pwa 모달 늦게보이는이슈, 홈랜딩 애니메이션 초기
    
    * fix: 로그인 랜딩페이지 png로 변경
    
    * feat: 이미지 폴더 이동

--- src/app/_constants/login-slides.ts
+++ src/app/_constants/login-slides.ts
@@ -1,24 +1,20 @@
-import randing1 from "../../lotties/randing 1.json";
-import randing2 from "../../lotties/randing 2.json";
-import randing3 from "../../lotties/randing 3.json";
-
 export const slides = [
   {
     title: "무비부키가 \n 당신의 취향을 저격해요",
     description:
       "좋아하는 콘텐츠를 기반으로 \n 내게 딱 맞는 이벤트를 추천받아요",
-    animation: randing1,
+    image: "/landing/randing 1.png",
   },
   {
     title: "복잡했던 대관 절차,\n 이제는 단 5분만에!",
     description:
       "원하는 이벤트를 손쉽게 만들고, 참여 인원 모집 \n부터 대관까지 빠르게 진행할 수 있어요",
-    animation: randing2,
+    image: "/landing/randing 2.png",
   },
   {
     title: "나만의 티켓을 발급하고\n 영화관에 방문해요!",
     description:
       "대관이 확정되면, 나만의 티켓이 발급돼요 \n 그날의 감동을 추억으로 남겨보세요",
-    animation: randing3,
+    image: "/landing/randing 3.png",
   },
 ];
