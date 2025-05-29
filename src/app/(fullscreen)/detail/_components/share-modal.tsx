b435ce2b050767f792fe446d47c8010a03b98e50
HYERYEON <100520490+anhyeryeon2@users.noreply.github.com>
2025-05-29 21:21:17 +0900

    [bug/#203]블러 아이콘 png 로 수정, 하단탭 안보이는 문제 해결 (#204)
    * fix: png 로 수정, 하단탭 안보이는 문제 해결
    
    * remove: 추가된 css 삭제

--- src/app/(fullscreen)/detail/_components/share-modal.tsx
+++ src/app/(fullscreen)/detail/_components/share-modal.tsx
@@ -2,9 +2,9 @@
 
 import { useEffect } from "react";
 import Modal from "../../../_components/modal";
-import { ShareKakaoIcon, ShareLinkIcon } from "@/icons/index";
+import { ShareKakaoIcon } from "@/icons/index";
 import { useToastStore } from "app/_stores/use-toast-store";
-
+import Image from "next/image";
 declare global {
   interface Window {
     Kakao: any;
@@ -83,7 +83,13 @@ export default function ShareModal({
           <ShareKakaoIcon />
         </button>
         <button onClick={handleCopyLink}>
-          <ShareLinkIcon />
+          <Image
+            src="/images/share-link.png"
+            alt="공유 아이콘"
+            width={70}
+            height={70}
+            priority
+          />
         </button>
       </div>
     </Modal>
--- src/app/(tabs)/layout.tsx
+++ src/app/(tabs)/layout.tsx
@@ -4,9 +4,11 @@ import { BottomNavigation } from "@/components";
 
 export default function TabsLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="scrollbar-hide mb-25.5 h-[calc(100vh-102px)] overflow-y-auto">
-      {children}
+    <>
+      <main className="scrollbar-hide h-[calc(100vh-102px)] overflow-y-auto">
+        {children}
+      </main>
       <BottomNavigation />
-    </main>
+    </>
   );
 }
