9cce992119f8851c96bd8432056e7b5dc727291e
Sohyunnnn <thgus05061@naver.com>
2025-08-18 13:54:03 +0900

    feat: date util 적용

--- src/app/(fullscreen)/event/success/_components/step2.tsx
+++ src/app/(fullscreen)/event/success/_components/step2.tsx
@@ -4,6 +4,7 @@ import DetailContent from "@/components/detail-content";
 import { useSelectedCinemaStore } from "app/_stores/use-selected-cinema-store";
 import { useEventFormStore } from "app/_stores/use-event-create-form";
 import { useUserStore } from "app/_stores/use-user-store";
+import { formatDateWithDay } from "@/utils/date-util";
 
 export default function Step2() {
   const { formData } = useEventFormStore();
@@ -12,9 +13,25 @@ export default function Step2() {
   );
   const user = useUserStore((state) => state.user);
 
+  const formattedFormData = {
+    ...formData,
+    eventDate: formData.eventDate ? formatDateWithDay(formData.eventDate) : "",
+    recruitmentStart: formData.recruitmentStart
+      ? formatDateWithDay(formData.recruitmentStart)
+      : "",
+    recruitmentEnd: formData.recruitmentEnd
+      ? formatDateWithDay(formData.recruitmentEnd)
+      : "",
+  };
+  console.log(formattedFormData);
+
   return (
     <div className="mt-3">
-      <DetailContent {...formData} {...(selectedCinema || {})} {...user} />
+      <DetailContent
+        {...formattedFormData}
+        {...(selectedCinema || {})}
+        {...user}
+      />
     </div>
   );
 }
