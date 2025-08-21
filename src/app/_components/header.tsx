1c5df6b85e92b0c2b5b9fe3f631699729b59fecf
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-08-21 22:12:22 +0900

    fix: header 아이콘 top 수정

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -37,13 +37,13 @@ export default function Header({
   return (
     <header
       className={cn(
-        "bg-gray-black fixed top-0 z-50 mt-4 flex h-[50px] w-full max-w-125 items-center justify-center focus-within:border-white",
+        "bg-gray-black fixed top-0 z-50 flex h-[70px] w-full max-w-125 items-center justify-center pt-0 focus-within:border-white",
         className,
       )}
     >
       {showBackButton && (
         <button
-          className="absolute top-2.5 left-2.5"
+          className="absolute top-6 left-2.5"
           onClick={handleBack}
           aria-label="뒤로가기"
           type="button"
@@ -56,7 +56,7 @@ export default function Header({
       {showCloseButton && (
         <button
           type="button"
-          className="absolute top-2.5 right-2.5"
+          className="absolute top-6 right-2.5"
           onClick={handleClose}
           aria-label="닫기"
         >
