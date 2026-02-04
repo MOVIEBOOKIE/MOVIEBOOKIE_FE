3eab358116d98701c0fbe10b6616e9834eb692e6
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2026-02-04 11:57:33 +0900

    refactor: 코드정리

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -5,9 +5,6 @@ dotenv.config({ path: ".env.test" });
 async function globalSetup(config: FullConfig) {
   const browser = await chromium.launch();
   const context = await browser.newContext();
-  // 인증 쿠키 설정
-  // 이 브라우저는 notification api 지원하지 않으니
-  // 다른 fcm테스트를 mock으로 대체 해야함
 
   await context.addCookies([
     {
@@ -21,8 +18,6 @@ async function globalSetup(config: FullConfig) {
   ]);
 
   // 쿠키가 적용되도록 페이지를 한번 로드
-  //그리고 인증상태를 스토리지에 저장
-
   const page = await context.newPage();
   await page.goto("http://localhost:3000");
 
