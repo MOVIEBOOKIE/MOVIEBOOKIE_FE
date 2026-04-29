fe731bbb952eae358b237a336e5e990688a2eed2
Sohyunnnn <thgus05061@naver.com>
2026-04-29 17:31:36 +0900

    style: 코드스타일 수정

--- src/app/(tabs)/layout.tsx
+++ src/app/(tabs)/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 export default function TabsLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <main className="scrollbar-hide app-shell min-h-[calc(100vh-102px)] overflow-y-auto pt-(--safe-top)">
+      <main className="scrollbar-hide app-shell pt-safe-top min-h-[calc(100vh-102px)] overflow-y-auto">
         {children}
       </main>
       <BottomNavigation />
