86b478f14bcdf7dd62498ee0440a313386d7e71d
Sohyunnnn <thgus05061@naver.com>
2026-05-13 16:34:41 +0900

    fix: 빌드 오류 수정

--- src/app/_apis/instance.ts
+++ src/app/_apis/instance.ts
@@ -18,10 +18,6 @@ const API_ORIGIN = process.env.NEXT_PUBLIC_VERCEL;
 
 const isServer = typeof window === "undefined";
 
-if (isServer && !API_ORIGIN) {
-  throw new Error("NEXT_PUBLIC_BASE_URL is required in server runtime");
-}
-
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: isServer ? new URL("/api", API_ORIGIN!).toString() : "/api",
   withCredentials: true,
